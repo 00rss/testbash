29e13d7a75c24e54a684f8ef39507e637c9c8d29
00rss <ronny-ricard1@hotmail.es>
2022-08-21 02:17:04 -0500

    first update

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#date +%d/%m/%Y
+#set #to see the current variables in this shell
+echo "Variable mal definida, error de orden no encontrada":
+#VAR = 1
+echo "Variables bien definidas:"
+VAR=1
+VAR1=2
+var=3
+echo "Variables: $VAR $VAR1 $var"
+echo "Variable VAR: $VAR"
+echo "Variable VAR1: $VAR1"
+echo "Variable VAR seguida de 1 (requiere llaves): ${VAR}1"
+echo "Comillas dobles: $VAR"
+echo 'Comillas simples: $VAR'
+echo "Valor: $VAR-1"
